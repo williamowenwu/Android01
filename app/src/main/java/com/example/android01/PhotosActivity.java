b135fe13ea89a6fa1fb7100ce2f493cdc12631c3
William Wu <willwu1217@gmail.com>
2024-04-23 16:54:12 -0400

    button for adding photos

--- app/src/main/java/com/example/android01/PhotosActivity.java
+++ app/src/main/java/com/example/android01/PhotosActivity.java
@@ -1,12 +1,19 @@
 package com.example.android01;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class PhotosActivity extends AppCompatActivity {
+
+    private static final int REQUEST_IMAGE_PICK = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +27,22 @@ public class PhotosActivity extends AppCompatActivity {
         TextView albumTitleTextView = findViewById(R.id.albumTitleTextView);
         albumTitleTextView.setText(albumName);
 
-        // Other setup code...
+        FloatingActionButton addPhotoButton = findViewById(R.id.addPhotoButton);
+        addPhotoButton.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(intent, REQUEST_IMAGE_PICK);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_PICK && resultCode == RESULT_OK && data != null) {
+            Uri selectedImageUri = data.getData();
+            // Use this URI to work with the selected image
+        }
     }
 
     public void onAlbumClicked(String selectedAlbumName) {
